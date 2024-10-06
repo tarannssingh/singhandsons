9bcbd40f24adf7247c171d6d3d8df21752f4b941
Taran Singh <tarannssingh@gmail.com>
2024-10-06 10:05:33 -0700

    feat: reset logging

--- src/api/admin.py
+++ src/api/admin.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 from src.api import auth
 import sqlalchemy
 from src import database as db
+import logging
+logger = logging.getLogger("uvicorn")
 
 router = APIRouter(
     prefix="/admin",
@@ -18,6 +20,7 @@ def reset():
     """
     with db.engine.begin() as connection:
         # reset gold
+        logger.info("reset")
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = 100, potion_capacity = 1, ml_capacity = 1, num_red_ml = 0, num_green_ml = 0, num_blue_ml = 0, num_dark_ml = 0"))
         connection.execute(sqlalchemy.text("UPDATE potion_inventory SET num_potions = 0"))    
         # connection.execute(sqlalchemy.text("UPDATE cart SET num_of_green_potions = 0, num_of_red_potions = 0, num_of_blue_potions = 0"))
