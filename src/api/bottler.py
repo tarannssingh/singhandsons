081aad636d88215391b69c9d3924739e77a2ec40
Taran Singh <tarannssingh@gmail.com>
2024-10-19 17:19:20 -0700

    feat: bootstrap

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -89,7 +89,7 @@ def get_bottle_plan():
                 to_bottle.append(
                     {
                         "potion_type": potion_blueprint,
-                        "quantity": 1,
+                        "quantity": 4,
                     }
                 )
                 red -= potion_blueprint[0]
