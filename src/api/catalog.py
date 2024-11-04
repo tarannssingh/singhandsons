9f2172a078011d6d7dbb24d296922ea9e3d37c08
Taran Singh <tarannssingh@gmail.com>
2024-11-03 18:05:09 -0800

    feat: catalog ledgerized

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -20,15 +20,30 @@ def get_catalog():
 
         # result = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory;")).fetchall()
         # num_green_price  = connection.execute(sqlalchemy.text("SELECT num_green_price FROM global_inventory;")).scalar()
-
-        potions = connection.execute(sqlalchemy.text("SELECT sku, red, green, blue, dark, name, num_potions, num_price FROM potion_catalog WHERE num_potions != 0;")).fetchall()
         
+        sql_to_execute = '''
+                    WITH 
+                    options as (
+                        SELECT potion_id, CAST(SUM(change) AS INT) as potions
+                        FROM potion_ledger_entries 
+                        GROUP BY potion_id
+                        HAVING CAST(SUM(change) AS INT) > 0
+                        ORDER BY potions DESC
+                        LIMIT 6
+                    )
+
+                    SELECT potion_catalog.sku, potion_catalog.red, potion_catalog.green, potion_catalog.blue, potion_catalog.dark, potion_catalog.name, options.potions, potion_catalog.num_price 
+                    FROM options 
+                    JOIN potion_catalog on options.potion_id = potion_catalog.id
+                    '''
+        
+        potions = connection.execute(sqlalchemy.text(sql_to_execute)).fetchall()
         logging.info("someone opened the catalog")
         # if we don't have any in stock, we shouldn't even consdier displaying, chipotle example
         catalog = []
         for potion in potions:
             catalog.append(
-                 {
+                {
                     "sku": potion[0],
                     "name": potion[5],
                     "quantity": potion[6],
